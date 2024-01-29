5f93d470cf9622dcbd08682673349f29b05beaaa
Hao Li <a136927177@126.com>
2024-01-29 13:35:28 +0800

    修改sim_lib.h与705保持一致

--- sim_lib.h
+++ sim_lib.h
@@ -2,12 +2,12 @@
 #define SIM_LIB_H
 
 #define CONFIG_TYPE_NUMS 2
-#define CONFIG_INT_NUMS 5
-#define CONFIG_FLOAT_NUMS 3
+#define CONFIG_INT_NUMS 2
+#define CONFIG_FLOAT_NUMS 12
 //#define CONFIG_CHAR_NUMS 20
 #define RESULT_TYPE_NUMS 2
 #define RESULT_INT_NUMS 4
-#define RESULT_FLOAT_NUMS 3
+#define RESULT_FLOAT_NUMS 4
 //#define RESULT_CHAR_NUMS 20
 
 enum class ProcStatus
@@ -25,7 +25,7 @@ struct ConfigStruct
 	ProcStatus command;
 	int idx;
 	int arg_int[3];//用户仿真模型的输入
-	float arg[3];
+	float arg[12];
 };
 
 //仿真输出参数
@@ -33,7 +33,7 @@ struct ResultStruct
 {
 	int idx;
 	int arg_int[3];
-	float arg_float[3];
+	float arg_float[4];
 };
 
 int main_run(float* config, int idx, ResultStruct* result);
